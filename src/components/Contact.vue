e2e8fa1ea340e8a10fca2808a0bcb8de23da1de2
pamnemes <pam.nemes@gmail.com>
2018-04-24 11:07:30 +0700

    emit pop

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -2,47 +2,17 @@
 <content-template>
 <template slot="header"> Content Us </template>
 content
-<div>
-  <div v-show="showform">
-  <div class="form-group">
-  <label for="usr">First Name:</label>
-  <input type="text" class="form-control" id="FName" v-model="form.FirstName">
-  <p>Message is: {{ form.FirstName }}</p>
-</div>
-<div class="form-group">
-  <label for="pwd">Last Name:</label>
-  <input type="text" class="form-control" id="Lname" v-model="form.LastName">
-    <p>Message is: {{ form.LastName }}</p>
-</div>
-<div class="form-group">
-  <label for="pwd">E-mail:</label>
-  <input type="email" class="form-control" id="Email" v-model="form.Email">
-  <p>Message is: {{ form.Email }}</p>
-</div>
-<div class="form-group">
-  <label for="comment">Comment:</label>
-  <textarea class="form-control" rows="5" id="comment" v-model="form.Commentb"></textarea>
-</div>
-      <!-- <p>alert:{{alerttext}}</p> -->
-<button type="button" class="btn btn-success" v-on:click="activeLinkNext()" v-bind:disabled="!enableSubmitBtn">Next</button>
-{{enableSubmitBtn}}
-</div>
-</div>
-  <div v-show="!showform">
-    <p>Name:{{fullname}}</p>
-    <p>Email: {{ form.Email }}</p>
-    <p>Comment: {{ form.Commentb }}</p>
-    <button type="button" class="btn btn-danger" v-on:click="activeLinkBlack()" >black</button>
-    <button type="button" class="btn btn-success">send</button>
+  <component v-bind:is="currentTabComponent" v-bind:is-preview.sync="isPreview" v-bind:form="form"></component>
+  <div>
   </div>
-  {{alertextbox}}
-  <p v-show="!enableSubmitBtn">กรอกข้อมูลไม่ครบ</p>
   </content-template>
 
 </template>
 
 <script>
 import ContentTemplate from '@/components/ContentTemplate'
+import ContactStep1 from '@/components/ContactStep1'
+import ContactStep2 from '@/components/ContactStep2'
 export default {
   name: 'Contact',
   data () {
@@ -50,14 +20,18 @@ export default {
       showform: true,
       form: {},
       alerttext: '',
-      disableButton: true
+      disableButton: true,
+      // currentTabComponent: 'ContactStep1',
+      isPreview: false
     }
   },
   components: {
     'text-word': {
       template: '<div>text word </div>'
     },
-    ContentTemplate
+    ContentTemplate,
+    ContactStep1,
+    ContactStep2
   },
   methods: {
     activeLinkNext: function () {
@@ -74,28 +48,11 @@ export default {
     }
   },
   computed: {
-    fullname: function () {
-      var fullname = ''
-      fullname = this.form.FirstName + ' ' + this.form.LastName
-      return fullname
-    },
-    enableSubmitBtn () {
-      if (this.form.FirstName && this.form.LastName && this.form.Email) {
-        return true
-      } else {
-        return false
-      }
+    currentTabComponent () {
+      var currentTabComponent = 'ContactStep1'
+      if (this.isPreview) currentTabComponent = 'ContactStep2'
+      return currentTabComponent
     }
-    // alertextbox () {
-    //   var alertextbox = ''
-    //   if (this.form.FirstName && this.form.LastName && this.form.Email) {
-    //     alertextbox = 'กรอกข้อมูลครบ'
-    //     return alertextbox
-    //   } else {
-    //     alertextbox = 'กรอกข้อมูลไม่ครบ'
-    //     return alertextbox
-    //   }
-    // }
   }
 }
 </script>
